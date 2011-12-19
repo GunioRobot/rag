434d2bfbd83390fd1b1e8968645ce1abe3e9908a
Gun.io Whitespace Robot <contact@gun.io>
2011-12-19 04:10:34 -0500

    Remove whitespace [Gun.io WhitespaceBot]

--- lib/form/BaseForm.class.php
+++ lib/form/BaseForm.class.php
@@ -2,10 +2,10 @@
 
 /**
  * Base project form.
- * 
+ *
  * @package    rag
  * @subpackage form
- * @author     Your name here 
+ * @author     Your name here
  * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
  */
 class BaseForm extends sfFormSymfony
